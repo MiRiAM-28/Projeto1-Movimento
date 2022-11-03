f88375ae3320e7c27323800b2c41d74cf1d649fc
MiRiAM-28 <mariabfox028@gmail.com>
2022-11-03 18:44:16 +0000

    Time 47526

---
+++ Assets/Script/FixedUpdateMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedUpdateMove : MonoBehaviour
+{
+    void FixedUpdate()
+    {
+        this.transform.Translate(0, 0, Time.deltaTime);
+    }
+}
