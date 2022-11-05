f0fd5e1e6e15e948624369d06f3153f0f4f9a28f
MiRiAM-28 <mariabfox028@gmail.com>
2022-11-05 19:18:37 +0000

    Calculating Trajectories 47526

--- Assets/Scripts/Shell.cs
+++ Assets/Scripts/Shell.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class Shell : MonoBehaviour
 {
     public GameObject explosion;
-    float mass = 10;
+
+    Rigidbody rb;
+
+    /*float mass = 10;
     float force = 300;
     float acceleration;
     float gravity = -9.8f;
     float gAccel;
     float speedZ;
-    float speedY;
+    float speedY;*/
 
     void OnCollisionEnter(Collision col)
     {
         if (col.gameObject.tag == "tank")
         {
             GameObject exp = Instantiate(explosion, this.transform.position, Quaternion.identity);
-            Destroy(exp, 0.5f);
+            Destroy(exp, 0.2f);
             Destroy(this.gameObject);
         }
     }
@@ -26,18 +29,19 @@ public class Shell : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        acceleration = force / mass;
+        /*acceleration = force / mass;
         speedZ += acceleration * Time.deltaTime;
         gAccel = gravity / mass;
         speedY += gAccel * Time.deltaTime;
         this.transform.Translate(0, speedY, speedZ);
 
-        force = 0;
+        force = 0;*/
+        this.transform.forward = rb.velocity;
     }
 }
--- Assets/Shoot.cs
+++ Assets/Shoot.cs
@@ -6,6 +6,11 @@ public class Shoot : MonoBehaviour
 {
     public GameObject shellPrefab;
     public GameObject shellSpawnPos;
+    public GameObject target;
+    public GameObject parent;
+    float speed = 15;
+    float turnSpeed = 2;
+    bool canShoot = true;
 
     // Start is called before the first frame update
     void Start()
@@ -13,15 +18,71 @@ public class Shoot : MonoBehaviour
         
     }
 
+    void CanShootAgain()
+    {
+        canShoot = true;
+    }
+
     void Fire()
     {
-        GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
+        if (canShoot)
+        {
+            GameObject shell = Instantiate(shellPrefab, shellSpawnPos.transform.position, shellSpawnPos.transform.rotation);
+            shell.GetComponent<Rigidbody>().velocity = speed * this.transform.forward;
+
+            canShoot = false;
+            Invoke("CanShootAgain", 0.5f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+            
+
+        Vector3 direction = (target.transform.position - parent.transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        parent.transform.rotation = Quaternion.Slerp(parent.transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
+
+        float? angle = RotateTurret();
+
+        if(angle !=null && Vector3.Angle(direction, parent.transform.forward) < 8)
             Fire();
+    }
+
+    float? RotateTurret()
+    {
+        float? angle = CalculateAngle(true);
+
+        if (angle!= null)
+        {
+            this.transform.localEulerAngles = new Vector3(360f - (float)angle, 0f, 0f);
+        }
+        return angle;
+    }
+
+    float? CalculateAngle(bool low)
+    {
+        Vector3 targetDir = target.transform.position - this.transform.position;
+        float y = targetDir.y;
+        targetDir.y = 0f;
+        float x = targetDir.magnitude;
+        float gravity = 9.81f;
+        float sSqr = speed * speed;
+        float underSqr = (sSqr * sSqr) - gravity * (gravity * x * x + 2 * y * sSqr);
+
+        if (underSqr >= 0f)
+        {
+            float root = Mathf.Sqrt(underSqr);
+            float highAngle = sSqr + root;
+            float lowAngle = sSqr - root;
+
+            if (low)
+                return (Mathf.Atan2(lowAngle, gravity * x) * Mathf.Rad2Deg);
+            else
+                return (Mathf.Atan2(highAngle, gravity * x) * Mathf.Rad2Deg);
+        }
+        else
+            return null;
     }
 }
