b2e549fa270560e86fee4ccd7615d1c8673d7892
MiRiAM-28 <mariabfox028@gmail.com>
2022-12-05 01:03:04 +0000

    City Crowd Maria Baptista 47526

---
+++ Assets/AIControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent agent;
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0.0f, 1.0f));
+        anim.SetTrigger("isWalking");
+        float sm = Random.Range(0.1f, 1.5f);
+        anim.SetFloat("speedMult", sm);
+        agent.speed *= sm;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (agent.remainingDistance < 1) {
+
+            agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
