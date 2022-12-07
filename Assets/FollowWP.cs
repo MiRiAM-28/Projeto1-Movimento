699b755bcc2a9735972a59cf8e4fc68e7bf22bf4
MiRiAM-28 <mariabfox028@gmail.com>
2022-12-07 23:54:45 +0000

    Following a Circuit Maria Baptista 47526

--- Assets/FollowWP.cs
+++ Assets/FollowWP.cs
@@ -9,26 +9,42 @@ public class FollowWP : MonoBehaviour
 
     public float speed = 10.0f;
     public float rotSpeed = 10.0f;
+    public float lookAhead = 10.0f;
+
+    GameObject tracker;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        tracker = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+        DestroyImmediate(tracker.GetComponent<Collider>());
+        tracker.GetComponent<MeshRenderer>().enabled = false;
 
+        tracker.transform.position = this.transform.position;
+        tracker.transform.rotation = this.transform.rotation;
+    }
+
+    void ProgressTracker()
+    {
+        if (Vector3.Distance(tracker.transform.position, this.transform.position) > lookAhead) return;
+        
+        if (Vector3.Distance(tracker.transform.position, waypoints[currentWP].transform.position) < 5)
+            currentWP++;
+
+        if (currentWP >= waypoints.Length)
+            currentWP = 0;
+
+        tracker.transform.LookAt(waypoints[currentWP].transform);
+        tracker.transform.Translate(0, 0, (speed + 1) * Time.deltaTime);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Vector3.Distance(this.transform.position, waypoints[currentWP].transform.position) < 3)
-            currentWP++;
+        ProgressTracker();
 
-        if(currentWP >= waypoints.Length)
-            currentWP = 0;
-
-        //this.transform.LookAt(waypoints[currentWP].transform);
-
-        Quaternion lookatWP = Quaternion.LookRotation(waypoints[currentWP].transform.position - this.transform.position);
+        Quaternion lookatWP = Quaternion.LookRotation(tracker.transform.position - this.transform.position);
 
         this.transform.rotation = Quaternion.Slerp(transform.rotation, lookatWP, rotSpeed * Time.deltaTime);
         
