c36b94c91431f9609e60ada14ba4d552cc08d459
MiRiAM-28 <mariabfox028@gmail.com>
2022-12-09 12:38:36 +0000

    Time1 Maria Baptista 47526

---
+++ Assets/Time/Script/LateUpdateMove.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LateUpdateMove : MonoBehaviour
+{
+    void LateUpdate()
+    {
+        this.transform.Translate(0, 0, 0.01f);
+    }
+}
---
+++ Assets/Time/Script/UpdateMove.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateMove : MonoBehaviour
+{
+
+    void Update()
+    {
+        this.transform.Translate(0, 0, 0.01f);
+    }
+}
